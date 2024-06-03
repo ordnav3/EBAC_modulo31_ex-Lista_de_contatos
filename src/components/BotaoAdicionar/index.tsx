709507f7bdcd1e2a3dbd1f80e39f7bf4328e10be
Evandro Santos Afonso <evandrosantosafonso@live.com>
2024-06-03 11:34:40 -0300

    setup inicial

--- src/components/BotaoAdicionar/index.tsx
+++
@@ -1,5 +0,0 @@
-import { Circulo } from './style'
-
-const BotaoAdicionar = () => <Circulo to="/novo">+</Circulo>
-
-export default BotaoAdicionar
--- src/styles/variaveis.ts
+++
@@ -1,6 +0,0 @@
-export default {
-  vermelho: '#c23616',
-  azul: '#1E90FF',
-  amarelo: '#f0c11a',
-  amarelo2: '#e1a32a'
-}
